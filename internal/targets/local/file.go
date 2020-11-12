b5b0548e315f1331168d6184e074dee085725f1a
Theo Salvo <buzzsurfr@gmail.com>
2020-11-11 21:11:50 -0500

    Add local file target

---
+++ internal/targets/local/file.go
@@ -0,0 +1,33 @@
+package local
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type File struct {
+	Path string
+	Name string
+	w    io.Writer
+}
+
+func NewFile(path, name string) *File {
+	// Ensure path exists
+	info, statErr := os.Stat(path)
+	if statErr != nil || !info.IsDir() {
+		os.MkdirAll(path, 0755)
+	}
+
+	// Create File
+	f, createErr := os.Create(filepath.Join(path, name))
+	if createErr != nil {
+		return &File{}
+	}
+
+	return &File{
+		Path: path,
+		Name: name,
+		w: f
+	}
+}
