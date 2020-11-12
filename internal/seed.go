6213b7a1e9aa4715398b5d4560672a4578c0a3c4
Theo Salvo <buzzsurfr@gmail.com>
2020-11-11 21:33:10 -0500

    Add seed funcs

--- internal/seed.go
+++ internal/seed.go
@@ -6,4 +6,26 @@ type Seed struct {
 	Name   string
 	source io.ReadCloser
 	target io.WriteCloser
+	closed bool
 }
+
+func NewSeed(name string, source io.ReadCloser, target io.WriteCloser) *Seed {
+	return &Seed{
+		Name:   name,
+		source: source,
+		target: target,
+		closed: false,
+	}
+}
+
+func (s *Seed) Copy() (written int64, err error) {
+	return io.Copy(s.target, s.source)
+}
+
+func (s *Seed) Close() error {
+	source.Close()
+	target.Close()
+	s.closed = true
+}
+
+type Seeds []Seed
